b0dc356afae8d3504b024523f7ba258712e7247e
shotaro yamamoto <shotoron@gmail.com>
2020-09-02 00:26:18 +0900

    [add]タスクの新規作成画面m編集画面を作成する

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only:[:show, :edit]
+  before_action :set_task, only:[:show, :edit, :update, :destroy]
   def index
     @tasks = Task.all
   end
@@ -8,13 +8,35 @@ class TasksController < ApplicationController
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to @task, notice: "タスク#{@task.title}を登録しました。"
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_url, notice: "タスク#{@task.title}を更新しました。"
+    else
+      render :edit
+    end
+  end
+
   private
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
