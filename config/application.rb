3ce023b357505bf9aa0b87e3d5a04c2ca9ff816c
shotaro yamamoto <shotoron@gmail.com>
2020-09-02 10:27:33 +0900

    [add]「一覧画面に遷移した場合、作成済みのタスク一覧が表示される」テストを作成

--- config/application.rb
+++ config/application.rb
@@ -16,13 +16,13 @@ module EveryTask
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
-    g.test_framework :rspec,
-                     model_specs: true,
-                     view_specs: false,
-                     helper_specs: false,
-                     routing_specs: false,
-                     controller_specs: false,
-                     request_specs: false
-  end
+      g.test_framework :rspec,
+                       model_specs: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+    end
   end
 end
