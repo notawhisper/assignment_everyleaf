a8705260b964d1af1ceb7262121653d18fe6ed01
shotaro yamamoto <shotoron@gmail.com>
2020-09-02 11:03:16 +0900

    [add]「タスクを新規作成したとき、作成したタスクが画面に表示される」「任意のタスク詳細画面に遷移したとき、該当タスクの内容が表示される」テストを作成

--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -1,8 +1,30 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  before do
+    # あらかじめタスク一覧のテストで使用するためのタスクを二つ作成する
+    FactoryBot.create(:task)
+    FactoryBot.create(:second_task)
+  end
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
+      # 1. new_task_pathに遷移する（新規作成ページに遷移する）
+      # ここにnew_task_pathにvisitする処理を書く
+      visit new_task_path
+      # 2. 新規登録内容を入力する
+      #「タスク名」というラベル名の入力欄と、「タスク詳細」というラベル名の入力欄にタスクのタイトルと内容をそれぞれ入力する
+      # ここに「タスク名」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+      fill_in 'Title', with: 'a'
+      # ここに「タスク詳細」というラベル名の入力欄に内容をfill_in（入力）する処理を書く
+      fill_in 'Description', with: 'aaa'
+      # 3. 「登録する」というvalue（表記文字）のあるボタンをクリックする
+      # ここに「登録する」というvalue（表記文字）のあるボタンをclick_onする（クリックする）する処理を書く
+      click_button 'Create Task'
+      # 4. clickで登録されたはずの情報が、タスク詳細ページに表示されているかを確認する
+      # （タスクが登録されたらタスク詳細画面に遷移されるという前提）
+      # ここにタスク詳細ページに、テストコードで作成したデータがタスク詳細画面にhave_contentされているか（含まれているか）を確認（期待）するコードを書く
+      expect(page).to have_content 'a'
       end
     end
   end
@@ -23,6 +45,10 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
+
+         task = FactoryBot.create(:task, title: 'task')
+         visit task_path(task)
+         expect(page).to have_content 'task'
        end
      end
   end
