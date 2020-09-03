78ffc6ac9c5ba12cfef6a5562171ac4e4bd6510e
shotaro yamamoto <shotoron@gmail.com>
2020-09-03 22:02:15 +0900

    [fix]テスト修正

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -5,11 +5,13 @@ FactoryBot.define do
   factory :task do
     title { 'Factoryで作ったデフォルトのタイトル１' }
     description { 'Factoryで作ったデフォルトのコンテント１' }
+    deadline { '2018-09-01 00:00:00' }
   end
   # 作成するテストデータの名前を「second_task」とします
   # （存在しないクラス名の名前をつける場合、オプションで「このクラスのテストデータにしてください」と指定します）
   factory :second_task, class: Task do
     title { 'Factoryで作ったデフォルトのタイトル２' }
     description { 'Factoryで作ったデフォルトのコンテント２' }
+    deadline { '2019-09-01 00:00:00' }
   end
 end
--- spec/system/task.spec.rb
+++ spec/system/task.spec.rb
@@ -6,11 +6,15 @@ RSpec.describe 'タスク管理機能', type: :system do
   end
 
   describe '新規作成機能' do
+    before do
+      visit new_task_path
+    end
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
-      visit new_task_path
       fill_in Task.human_attribute_name(:title), with: 'しんきさくせい'
       fill_in Task.human_attribute_name(:description), with: 'aaa'
+      # binding.irb
+      fill_in 'Deadline', with: "002020010122222"
       click_on '登録する'
       expect(page).to have_content 'しんきさくせい'
       end
