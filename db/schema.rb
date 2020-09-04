78e0475efe9ea1e288b97fb09f5f744363ca82c4
shotaro yamamoto <shotoron@gmail.com>
2020-09-04 22:37:19 +0900

    [add]ユーザーモデルを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_064044) do
+ActiveRecord::Schema.define(version: 2020_09_04_132408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2020_09_04_064044) do
     t.string "status", default: "未着手"
     t.integer "priority", default: 0, null: false
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
