4123dcd7fb2bc6c1108410ad56b8ef9b2c44dc91
shotaro yamamoto <shotoron@gmail.com>
2020-09-04 15:42:52 +0900

    [add]Taskモデルにpriority属性を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_020426) do
+ActiveRecord::Schema.define(version: 2020_09_04_064044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_04_020426) do
     t.datetime "updated_at", null: false
     t.datetime "deadline", null: false
     t.string "status", default: "未着手"
+    t.integer "priority", default: 0, null: false
     t.index ["title"], name: "index_tasks_on_title"
   end
 
