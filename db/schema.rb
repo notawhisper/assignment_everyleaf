761669f3d1fea03a51ef47cea15ea71c453b77a0
shotaro yamamoto <shotoron@gmail.com>
2020-09-04 11:06:45 +0900

    [add]タスクテーブルのtitleカラムにindexを付与

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_153055) do
+ActiveRecord::Schema.define(version: 2020_09_04_020426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_153055) do
     t.datetime "updated_at", null: false
     t.datetime "deadline", null: false
     t.string "status", default: "未着手"
+    t.index ["title"], name: "index_tasks_on_title"
   end
 
 end
