d7eac0245d95cc8162fef163123474cb59bb7cca
shotaro yamamoto <shotoron@gmail.com>
2020-09-07 21:36:22 +0900

    [add]Admin::UsersControllerを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'users/new'
+    get 'users/edit'
+    get 'users/show'
+    get 'users/index'
+  end
   root "tasks#index"
   resources :users
   get '/login', to: 'sessions#new'
